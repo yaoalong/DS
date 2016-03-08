593941a8e3c53f081b14f7bc19f274df78334fb0
yaoalong <sishimei@163.com>
2016-03-08 13:50:09 +0800

    修复:单个机器的不同的zab应该含有不同的处理ip

--- replication-zab/src/main/java/org/lab/mars/onem2m/server/quorum/M2mQuorumPeerMain.java
+++ replication-zab/src/main/java/org/lab/mars/onem2m/server/quorum/M2mQuorumPeerMain.java
@@ -1,20 +1,3 @@
-/**
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- * <p>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package org.lab.mars.onem2m.server.quorum;
 
 import java.io.IOException;
@@ -83,7 +66,6 @@ public class M2mQuorumPeerMain extends Thread {
         if (args.length == 1) {
             config.parse(args[0]);
         }
-        System.out.println("hha");
         if (args.length == 1 && config.servers.size() > 0) {
             runFromConfig(config, args);
         } else {
@@ -116,7 +98,6 @@ public class M2mQuorumPeerMain extends Thread {
 
             ZooKeeperRegister zooKeeperRegister = new ZooKeeperRegister();
             // TODO zooKeeperRegister.starter(args, networkPool);
-            System.out.println("哈市");
             // TODO zooKeeperRegister.register(address + ":" + webPort);
             List<M2mQuorumPeer> quorumPeers = new ArrayList<M2mQuorumPeer>();
             long minValue = config.isTemporyAdd() ? 1
@@ -156,8 +137,8 @@ public class M2mQuorumPeerMain extends Thread {
                 quorumPeer.setDataDir(config.getDataDir());
                 quorumPeer.setDataLogDir(config.getDataLogDir());
                 quorumPeer.setQuorumPeerMain(this);
-                quorumPeer.setRangeDOs(networkPool.getRanges(config.getMyIp()
-                        + ":" + config.clientPort));
+                quorumPeer.setRangeDOs(networkPool.getRanges(quorumPeer
+                        .getHandleIp()));
 
                 quorumPeer.setMyIp(config.getMyIp());
 
